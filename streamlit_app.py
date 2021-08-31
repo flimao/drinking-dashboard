f1408c63f0ecd32343f7fdb9e2343dcd65ea36e4
Limão <dev@lmnice.me>
2021-08-31 17:47:55 -0300

    Tornar mais divertido o app hello world.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,31 @@
+import time
 import streamlit as st
+import plotly 
 
-st.title('Hello world!')
+
+st.title('Hello, *World!* :sunglasses:')
+
+st.header('How are you today?')
+st.caption('Fine, I guess?..')
+st.write('<br><br>', unsafe_allow_html = True)
+
+st.text('Some code:')
+st.code('''
+st.title('Hello, *World!* :sunglasses:')
+
+st.header('How are you today?')
+st.caption('Fine, I guess?..')
+''')
+
+st.write('<br><br>', unsafe_allow_html = True)
+
+st.write('Legen...')
+with st.spinner('...wait for it...'):
+    time.sleep(5)
+st.success('...dary!')
+
+# cards
+avg_mm = 0.7
+
+cols = st.columns(3)
+cols[1].metric('Variação MoM', f'{avg_mm:.0%}', f'{avg_mm - 1:.0%}')
