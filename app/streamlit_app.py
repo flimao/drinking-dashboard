5080aabae845294620ca55eb4ccb67969e62cb26
Felipe Oliveira <flimao@petrobras.com.br>
2021-09-03 23:28:45 -0300

    Atualizar imagem de fundo.

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,8 +1,29 @@
+import base64
 import time
 from scipy.stats import sem
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/jpeg;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
 
 dfrel = pd.read_csv(r'datasets/religiao-tratado.csv')
 
@@ -24,7 +45,7 @@ df_full = (df_raw
     })
 )
 df = df_full[(df_full.Year > 1960) & (df_full.Year <= 2011)]
-df['MajorReligion_top'] = topnrels(5)
+df['MajorReligion_top'] = topnrels(6)
 df['Total_BottlesWinePerMonth'] = df['Total_LitersAlcPerYear'] * (1 / (0.12 * 0.75 * 12))
 
 # labels das colunas, para rotular nos gráficos
@@ -42,6 +63,15 @@ labels = {
 # construção do mapa de cores consistente em todos os gráficos
 regions = df['Region'].unique()
 color_palette = px.colors.qualitative.G10
+color_palette = [
+            "#348ABD",
+            "#A60628",
+            "#7A68A6",
+            "#467821",
+            "#CF4457",
+            "#188487",
+            "#E24A33"
+  ]
 
 region_colors = { region: color for region, color in zip(regions, color_palette[:len(regions)]) }
 
@@ -55,14 +85,15 @@ conclusao = r'**_Pontos notáveis_**:'
 
 # css para tornar um pouco mais largo
 
+set_png_as_page_bg('app/res/chalkboard2.jpg')
 st.markdown(
         f"""
-<style>
-    .reportview-container .main .block-container{{
-        max-width: 65%;
-    }}
-</style>
-""",
+        <style>
+            .reportview-container .main .block-container{{
+                max-width: 65%;
+            }}
+        </style>
+        """,
         unsafe_allow_html=True,
     )
 
@@ -96,12 +127,17 @@ alcline.update_traces(
 
 alcline.update_layout(
     autosize=False,
-    width=1100,
+    width=5000,
     height=600,
     hovermode = 'x unified',
+    paper_bgcolor = 'rgba(0,0,0,0)',
+    plot_bgcolor = 'rgba(0,0,0,0)',
+    hoverlabel_bgcolor = 'rgba(20,20,20,0.7)',
+    xaxis_gridcolor = 'rgba(255,255,255,0.0)',
+    yaxis_gridcolor = 'rgba(255,255,255,0.1)',
 )
 
-st.plotly_chart(alcline)
+st.plotly_chart(alcline, use_container_width = True)
 
 st.success(fr'''{conclusao}
 A Europa é a maior consumidora de bebidas alcoólicas, bebendo em média 40% a mais que as Américas (2ª maior consumidora).
@@ -132,17 +168,21 @@ barrel.update_traces(
     '<b>Religião: %{x}</b><br>' +
     labels['Total_BottlesWinePerMonth'] +': %{y:.1f} garrafas'+
     '<extra></extra>',
-    error_y_color = color_palette[-3],
+    error_y_color = color_palette[-1],
     error_y_thickness = 3
 )
 barrel.update_layout(
     autosize=False,
-    width=1100,
+    width=5000,
     height=600,
-    xaxis = {'categoryorder': 'total descending'}
+    xaxis = {'categoryorder': 'total descending'},
+    hoverlabel_bgcolor = 'rgba(20,20,20,0.7)',
+    paper_bgcolor = 'rgba(0,0,0,0)',
+    plot_bgcolor = 'rgba(0,0,0,0)',
+    yaxis_gridcolor = 'rgba(255,255,255,0.1)',
 )
 
-st.plotly_chart(barrel)
+st.plotly_chart(barrel, use_container_width = True)
 
 st.success(rf'''{conclusao} 
 As pessoas que se dizem **Católicos Romanos** (ou equivalente), **Budistas** ou de outras religiões são as que mais consomem bebidas alcoólicas, 
@@ -150,7 +190,7 @@ consumindo cada um o equivalente a entre 2 e 4.5 garrafas de vinho por mês.
     
 **Anglicanos** bebem substancialmente menos que Católicos Romanos, consumindo por volta de 1.5 garrafa de vinho por mês por pessoa.
 
-As pessoas que se dizem sem religião tem o consumo de bebidas bastante variável; algumas consomem bastante e outras não consomem nenhuma bebida alcoólica.
+As pessoas que se dizem **Católicas Ortodoxas**, bem como pessoa que se dizem **sem religião**, tem o consumo de bebidas bastante variável; algumas consomem bastante e outras não consomem nenhuma bebida alcoólica.
 ''')
 
 ### relação entre consumo de bebidas alcoólicas e expectativa de vida
@@ -164,13 +204,21 @@ sct = px.scatter(df, y = 'LifeExp', x = 'Total_BottlesWinePerMonth',
     labels = labels
 )
 
-sct.update_layout(
-    autosize=False,
-    width=1100,
-    height=600,
+sct.update_traces(
+    marker_size = 10,
 )
 
-st.plotly_chart(sct)
+sct.update_layout(
+    autosize=False,
+    width=5000,
+    height=600,
+    paper_bgcolor = 'rgba(0,0,0,0)',
+    plot_bgcolor = 'rgba(0,0,0,0)',
+    xaxis_gridcolor = 'rgba(255,255,255,0.3)',
+    yaxis_gridcolor = 'rgba(255,255,255,0.3)',
+)
+
+st.plotly_chart(sct, use_container_width = True)
 
 st.info(fr'Parece que não há uma relação muito clara entre a quantidade de álcool consumida e a expectativa de vida, mesmo se separarmos por região.')
 
@@ -189,10 +237,16 @@ mapa.update_layout(
     autosize=False,
     width=1100,
     height=600,
-    title = 'Consumo de bebidas alcoólicas (em garrafas de vinho equivalentes por mês por pessoa, entre 1960 e 2011)' 
+    title = 'Consumo de bebidas alcoólicas (em garrafas de vinho equivalentes por mês por pessoa, entre 1960 e 2011)',
+    paper_bgcolor = 'rgba(0,0,0,0)',
+    plot_bgcolor = 'rgba(0,0,0,0)',
 )
 
-st.plotly_chart(mapa)
+mapa.update_geos(
+    oceancolor = 'blue'
+)
+
+st.plotly_chart(mapa, use_container_width = True)
 
 st.success(fr'''{conclusao}
 Até meados da década de 80, A Austrália, o Chile e a Europa como um todo consumiam grandes quantidades de bebidas alcoólicas. Desde então, ...
